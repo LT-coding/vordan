daa0efee46714b69da17c496c0b2ffe98f69a9e9
lLalvar <mnacakanyan.lalvar@gmail.com>
2024-06-22 09:15:23 +0400

    refactor: Update Stories component to display story title below the image

--- src/app/_home/stories.tsx
+++ src/app/_home/stories.tsx
@@ -4,12 +4,14 @@ export default function Stories() {
   return (
     <div className='flex gap-16 justify-center pt-28'>
       {storiesMockData.map((story) => (
-        <img
-          key={story.id}
-          src={story.imageUrl}
-          alt='story'
-          className='bg-gray-200 rounded-full size-28 object-cover'
-        />
+        <div key={story.id} className='flex flex-col gap-2 items-center'>
+          <img
+            src={story.imageUrl}
+            alt='story'
+            className='bg-gray-200 rounded-full size-28 object-cover'
+          />
+          <div className='text-xl'>{story.title}</div>
+        </div>
       ))}
     </div>
   )
